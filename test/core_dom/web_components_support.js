deeb5b0528de44f331b27cf06b82a19556797a7b
Victor Berchet <victor@suumit.com>
2014-08-20 10:47:41 +0200

    test++

--- test/core_dom/web_components_support.js
+++ test/core_dom/web_components_support.js
@@ -1,18 +1,27 @@
 /**
  * Used to create Javascript Web Components from Dart tests
  */
-function angularTestsRegisterElement(name, _prototype) {
+function angularTestsRegisterElement(name, prototype) {
   // Polymer requires that all prototypes are chained to HTMLElement
   // https://github.com/Polymer/CustomElements/issues/121
-  _prototype = _prototype || {};
-  function F() {}
-  F.prototype = HTMLElement;
-  var prototype = new F();
-  for (var p in _prototype) {
-    if (_prototype.hasOwnProperty(p)) {
-      prototype[p] = _prototype[p];
+  //_prototype = _prototype || {};
+  //function F() {}
+  //F.prototype = HTMLElement;
+  //var prototype = new F();
+  //for (var p in _prototype) {
+  //  if (_prototype.hasOwnProperty(p)) {
+  //    prototype[p] = _prototype[p];
+  //  }
+  //}
+  //prototype.__proto__ = HTMLElement.prototype;
+  var proto = Object.create(HTMLElement.prototype);
+  for (var p in prototype) {
+    if (prototype.hasOwnProperty(p)) {
+      proto[p] = prototype[p];
     }
   }
+
+
   prototype.createdCallback = function() {};
-  document.registerElement(name, {prototype: prototype});
+  document.registerElement(name, {prototype: proto});
 }
